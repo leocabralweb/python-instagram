374bbf96ca48cae9d6108385a9a91a33f138ed4a
Leonardo Cabral <leonardo.cabral@stormsec.com.br>
2013-08-15 08:26:30 -0300

    add min_tag_id to accepts_parameters of tag_recent_media.

--- instagram/client.py
+++ instagram/client.py
@@ -145,7 +145,7 @@ class InstagramAPI(oauth2.OAuth2API):
 
     tag_recent_media = bind_method(
                 path="/tags/{tag_name}/media/recent",
-                accepts_parameters=MEDIA_ACCEPT_PARAMETERS + ['tag_name'],
+                accepts_parameters=MEDIA_ACCEPT_PARAMETERS + ['tag_name', 'min_tag_id'],
                 root_class=Media,
                 paginates=True)
 
